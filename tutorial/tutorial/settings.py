9c25dac315302c9f9c7607d094adcfceccddcdf1
Richard Hagen <richard.hagen@gmail.com>
2021-11-05 23:22:40 +0100

    add Spectacular API documentation

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'snippets.apps.SnippetsConfig',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,18 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    # OTHER SETTINGS
+}
 
 ROOT_URLCONF = 'tutorial.urls'
 
